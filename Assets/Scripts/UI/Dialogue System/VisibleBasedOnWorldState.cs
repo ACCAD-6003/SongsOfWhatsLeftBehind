98557da3a6f12b9ee3611252488d1a5e9fe892fc
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-04-09 23:09:56 -0400

    Hid Garrett when not yet seen

---
+++ Assets/Scripts/UI/Dialogue System/VisibleBasedOnWorldState.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace UI.Dialogue_System
+{
+    public class VisibleBasedOnWorldState : MonoBehaviour
+    {
+        [SerializeField] private string key;
+        [SerializeField] private int value;
+        [SerializeField] private bool invert;
+        
+        private void Start()
+        {
+            WorldState.OnWorldStateChanged += UpdateVisibility;
+            UpdateVisibility();
+        }
+
+        private void OnDestroy()
+        {
+            WorldState.OnWorldStateChanged -= UpdateVisibility;
+        }
+
+        private void UpdateVisibility()
+        {
+            var shouldBeVisible = WorldState.GetState(key) == value;
+            if (invert)
+            {
+                shouldBeVisible = !shouldBeVisible;
+            }
+            
+            gameObject.SetActive(shouldBeVisible);
+        }
+    }
+}
