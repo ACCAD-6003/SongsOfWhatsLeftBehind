8acd78b926d187bc891ae1e87bd30531373a349c
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-04-17 15:15:43 -0400

    Music Fade

--- Assets/Scripts/RhythmGame/MuteOnRhythmGame.cs
+++ Assets/Scripts/RhythmGame/MuteOnRhythmGame.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace RhythmGame
@@ -7,8 +9,10 @@ namespace RhythmGame
     public class MuteOnRhythmGame : MonoBehaviour
     {
         [SerializeField] RhythmGameManager rhythmGameManager;
+        [SerializeField] float fadeDuration = 0.5f; 
 
         private AudioSource source;
+        private Coroutine fadeCoroutine;
 
         private void Awake()
         {
@@ -17,22 +21,52 @@ namespace RhythmGame
 
         private void Start()
         {
+            source.volume = 0;
+            fadeCoroutine = StartCoroutine(FadeHandler(1));
+            SceneTools.onSceneTransitionStart += Mute;
+            if (rhythmGameManager == null) return;
             rhythmGameManager.OnSongStart += Mute;
             rhythmGameManager.OnSongEnd += Unmute;
+        }
+
+        private void Mute()
+        {
+            StopCoroutine(fadeCoroutine);
+            fadeCoroutine = StartCoroutine(FadeHandler(0));
         }
         
         private void Mute(SongData _)
         {
-            source.volume = 0;
+            Mute();
+        }
+
+        private void Mute(int _)
+        {
+            Mute();
+        }
+        
+        private IEnumerator FadeHandler(float target)
+        {
+            var startTime = Time.time;
+            var startVolume = source.volume;
+            
+            while (Time.time - startTime < fadeDuration)
+            {
+                source.volume = Mathf.Lerp(startVolume, target, (Time.time - startTime) / fadeDuration);
+                yield return null;
+            }
         }
         
         private void Unmute()
         {
-            source.volume = 1;
+            StopCoroutine(fadeCoroutine);
+            fadeCoroutine = StartCoroutine(FadeHandler(1));
         }
         
         private void OnDestroy()
         {
+            SceneTools.onSceneTransitionStart -= Mute;
+            if (rhythmGameManager == null) return;
             rhythmGameManager.OnSongStart -= Mute;
             rhythmGameManager.OnSongEnd -= Unmute;
         }
